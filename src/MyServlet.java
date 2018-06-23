14f1cbb6440f42eacd26c1d783b8836495edb030
Valer <Valeris2812@mail.ru>
2018-06-23 11:39:15 +0200

    add index.html new info. add bootstrap

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -6,14 +6,22 @@ public class MyServlet extends javax.servlet.http.HttpServlet {
 
 protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
     response.setCharacterEncoding("UTF-8"); ///  киррилица
+
     String name = request.getParameter("firstName");
     String suName = request.getParameter("secondName");
+    String job = request.getParameter("job");
+    //String [] jobs = request.getParameterValues("job"); Если нам необходимо принять из поля несколько значений
+
+    String gender = request.getParameter("gender");
+    String remember = request.getParameter("remember");
+
     System.out.println(name = name  + " " + suName);
 
     response.setContentType("text/html");  /// Для того, что бы работала
     //response.setCharacterEncoding("UTF-8"); ///  киррилица
     PrintWriter out = response.getWriter();
-    out.print("<h1> Darova pacan, " + name + " " + suName + " </h1><br>!");
+    out.print("<title>Здравствуй," + name + "</title> ");
+    out.print("<h1> Ваша введенная информация:, " + job + " " + gender + " </h1><br>!");
     out.print("<form method= \"get\" action=\"CalsSalary\"> ");
     out.print("<input type=\"submit\" value=\"Отправить\"> </form>");
     out.close();
